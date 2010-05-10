8e627bc13e7292263cee4ccb152b585db058cbae
sharon_correll <sharon_correll@454cffdb-57ea-0310-8ee2-a0481aacfdbf>
2010-05-10 19:07:31 +0000

    Cast to utf16 and LPCWSTR.

--- grcompiler/trunk/compiler/Generic/Platform.cpp
+++ grcompiler/trunk/compiler/Generic/Platform.cpp
@@ -9,34 +9,36 @@ namespace gr
 
 size_t Platform_UnicodeToANSI(const utf16 * prgchwSrc, size_t cchwSrc, char * prgchsDst, size_t cchsDst)
 {
-	return ::WideCharToMultiByte(CP_ACP, 0, prgchwSrc, cchwSrc, prgchsDst, cchsDst, NULL, NULL);
+	return ::WideCharToMultiByte(CP_ACP, 0, (LPCWSTR)prgchwSrc, cchwSrc, prgchsDst, cchsDst, NULL, NULL);
 }
 
 size_t Platform_AnsiToUnicode(const char * prgchsSrc, size_t cchsSrc, utf16 * prgchwDst, size_t cchwDst)
 {
-	return ::MultiByteToWideChar(CP_ACP, 0, prgchsSrc, cchsSrc, prgchwDst, cchwDst);
+	return ::MultiByteToWideChar(CP_ACP, 0, prgchsSrc, cchsSrc, (LPWSTR)prgchwDst, cchwDst);
 }
 
 size_t Platform_8bitToUnicode(int nCodePage, const char * prgchsSrc, int cchsSrc,
 	utf16 * prgchwDst, int cchwDst)
 {
-	return ::MultiByteToWideChar(nCodePage, 0, prgchsSrc, cchsSrc, prgchwDst, cchwDst);
+	return ::MultiByteToWideChar(nCodePage, 0, prgchsSrc, cchsSrc, (LPWSTR)prgchwDst, cchwDst);
 }
 
 size_t utf8len(const char *s)
 {
-	return mbstowcs(NULL,s,0);
+	return mbstowcs(NULL, s, 0);
 }
 
 
 utf16 *utf16cpy(utf16 *dest, const utf16 *src)
 {
-	return wcscpy(dest, src);
+//	return wcscpy(dest, src);
+	return (utf16*)wcscpy((wchar_t*)dest, (const wchar_t*)src);
 }
 
 utf16 *utf16ncpy(utf16 *dest, const utf16 *src, size_t n)
 {
-	return wcsncpy(dest, src, n);
+//	return wcsncpy(dest, src, n);
+	return (utf16*)wcsncpy((wchar_t*)dest, (const wchar_t*)src, n);
 }
 
 utf16 *utf16ncpy(utf16 *dest, const char *src, size_t n)
@@ -51,17 +53,22 @@ utf16 *utf16ncpy(utf16 *dest, const char *src, size_t n)
 //size_t wcslen(const wchar_t *s);
 size_t utf16len(const utf16 *s)
 {
-	return wcslen(s);
+//	return wcslen(s);
+	return wcslen((const wchar_t*)s);
+
 }
 
 int utf16cmp(const utf16 *s1, const utf16 *s2)
 {
-	return wcscmp(s1, s2);
+//	return wcscmp(s1, s2);
+	return wcscmp((const wchar_t*)s1, (const wchar_t*)s2);
+
 }
 
 int utf16ncmp(const utf16 *s1, const utf16 *s2, size_t n)
 {
-	return wcsncmp(s1, s2, n);
+//	return wcsncmp(s1, s2, n);
+	return wcsncmp((const wchar_t*)s1, (const wchar_t*)s2, n);
 }
 
 
