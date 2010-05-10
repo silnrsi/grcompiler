83dde56e1fad92c476306bc0fa221b14767eb164
sharon_correll <sharon_correll@454cffdb-57ea-0310-8ee2-a0481aacfdbf>
2010-05-10 19:54:41 +0000

    Make integer-width typedefs portable - from Neil Mayhew.
    __int8_t_defined is a Linux-only construct, and so not suitable
    as a conditional compilation switch for Unix systems in general.
    
    Only the int32 and uint32 type are problematic, but this can be
    solved for most platforms by using 'int' instead of 'long'.

--- grcompiler/trunk/compiler/Generic/GrPlatform.h
+++ grcompiler/trunk/compiler/Generic/GrPlatform.h
@@ -45,7 +45,6 @@
 
 // Project headers
 #ifndef _WIN32
-#include <stdint.h>
 #include "GrMstypes.h"
 #else
 #include <algorithm>
@@ -79,27 +78,12 @@ namespace std
 namespace gr
 {
 // Typedefs
-#ifdef __int8_t_defined
-typedef uint32_t utf32;	// UTF32 encoded Unicode codepoints
-typedef uint16_t utf16;	// UTF16 encoded Unicode codepoints
-typedef uint16_t gid16;	// glyph ID
-typedef uint16_t data16;	// generic 16-bit data
-typedef uint32_t fontTableId32;	// ID to pass to getTable()
-#else
-// Typedefs
-typedef unsigned long int utf32;	// UTF32 encoded unicode codepoints.
-typedef unsigned short int utf16;	// UTF16 encoded unicode codepoints.
-typedef unsigned short int gid16;	// glyph ID
-typedef unsigned short int data16;	// generic 16-bit data
-typedef unsigned long int fontTableId32;	// ID to pass to getTable()
-#endif
-
-//typedef unsigned char		uchar;
-//typedef unsigned int		uint;
-//typedef unsigned short		ushort;
-//typedef unsigned long		ulong;
-//typedef __int64 			int64; // Hungarian: lln
-//typedef unsigned __int64	uint64; // Hungarian llu
+// These are correct for the x86_64 architecture too, on both Windows and Unix
+typedef unsigned       int utf32;          // UTF32 encoded Unicode codepoints
+typedef unsigned short int utf16;          // UTF16 encoded Unicode codepoints
+typedef unsigned short int gid16;          // glyph ID
+typedef unsigned short int data16;         // generic 16-bit data
+typedef unsigned       int fontTableId32;  // ID to pass to getTable()
 
 #ifndef NO_ASM
 #define NO_ASM
