c16ff5b3ffc0bea0e529e9f643d3095d7eb590e3
DALLAS\correll <sharon_correll@sil.org>
2019-07-01 14:30:43 -0500

    Update copyright date.

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -153,7 +153,7 @@ int main(int argc, char * argv[])
 		#endif
 		// \xc2\xa9 = copyright symbol
 		std::cout << "\n"
-			<< "Copyright (c) 2002-2018, by SIL International.  All rights reserved.\n";
+			<< "Copyright (c) 2002-2019, by SIL International.  All rights reserved.\n";
 	}
 
 	if (argc < 3 + cargExtra)
